a706cac4164fd1bcbafa0c390e1f71a3999bcb57
sandesh93 <sandeshrane93@gmail.com>
2018-11-26 15:24:03 +0530

    Updated Value diff as per GoldenGate prod config

--- ValueDiffUpdate.ps1
+++ ValueDiffUpdate.ps1
@@ -1,22 +1,35 @@
-﻿##Note: If change in field location in original file this script will not genrate desired output.
-##Script developed in 26th November-2018 
+﻿##Note: If change in field location (RBA) in original config file will not give desired output.
+##Script developed in 26th November-2018
+#### Purpose of the script: Get the RBA count from config file and everytime check if RBA count is changing . if yes , calculate the difference by which it got changed. 
+#### further it should compare it with updated value.
 
+$BASE_DIR="D:\\goldengateCustomScript"
 ###Original File path from where data to be extracted.
-$File="D:\\temp\\OriginalFile.txt"
+$ORIGINAL_FILE="D:\\goldengateCustomScript\\goldengate.txt"
  
 ###### Old data will be moved to below file for new comparision.
-$tempfile="D:\\temp\\comparefile.txt"
+$TEMP_FILE="D:\\goldengateCustomScript\\comparefile.txt"
 
 #Extract required field value and post it into tempfile if temp file is empty.
 
+##Check if tmp file exists if not then create.
+
+if (!(Test-Path $TEMP_FILE))
+{
+   New-Item -path $BASE_DIR -name comparefile.txt -type "file" -value ""
+   Write-Host "temp file not present hence created."
+
+}
+
+## CHeck if File is empty . if yes then write content from Original file for comparision
 If ((Get-Content $tempfile ) -eq $Null) {
-    (Get-Content $File)[3].Split(" ")[5] | out-file -FilePath $tempfile
-    write-host "File was blank... Resetting Difference value to Zero."
+    ((Get-Content $ORIGINAL_FILE)[15].Trim()).Split(" ")[3] | out-file -FilePath $TEMP_FILE
+    write-host "File was blank. Resetting Difference value to Zero."
     $diference = 0
-    exit
+    
   }
 
-$compare=(Get-Content $File)[3].Split(" ")[5]
+$compare=((Get-Content $ORIGINAL_FILE)[15].Trim()).Split(" ")[3]
 write-host "Latest Value= " $compare 
 $oldvalue=(Get-Content $tempfile)
 write-host "Old Value in File= " $oldvalue 
@@ -39,7 +52,7 @@ write-host "Values Changed."
 $diference= $compare - $oldvalue
 write-host "Difference in values:" ($compare - $oldvalue)
 write-host "Updated to temp file for next comparision."
-(Get-Content $File)[3].Split(" ")[5] | out-file -FilePath $tempfile
+((Get-Content $ORIGINAL_FILE)[15].Trim()).Split(" ")[3] | out-file -FilePath $TEMP_FILE
 echo "----------------------"
 
 }
